cbab11f25e5e34122d4a31641def25ace59735dd
Jose David <lujan9495@gmail.com>
2024-05-08 22:47:48 -0500

    Creando el Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import './index.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Spotify Clone</h1>
-    </>
+    <div className="customButton relative h-screen p-2 gap-2">
+      <aside className='[grid-area:aside] flex-col flex-auto overflow-y-auto bg-blue-100 '>
+        ASIDE
+      </aside>
+
+      <main className='[grid-area:main] rounded-lg overflow-y-auto bg-zinc-900'>
+        MAIN
+      </main>
+
+      <footer className='[grid-area:player] min-h-[100px]'>
+        
+      </footer>
+    </div>
   )
 }
 
