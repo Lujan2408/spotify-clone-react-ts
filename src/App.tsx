ea1bcdf8f3b4382ad7e97ad19e75fa196359de4d
Jose David <lujan9495@gmail.com>
2024-05-09 00:10:10 -0500

    Part of Aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import AsideMenu from './components/AsideMenu'
 import './index.css'
 
 function App() {
 
   return (
     <div className="customButton relative h-screen p-2 gap-2">
-      <aside className='[grid-area:aside] flex-col flex-auto overflow-y-auto bg-blue-100 '>
-        ASIDE
+      <aside className='[grid-area:aside] flex-col flex-auto overflow-y-auto  '>
+        <AsideMenu />
       </aside>
 
       <main className='[grid-area:main] rounded-lg overflow-y-auto bg-zinc-900'>
-        MAIN
+        
       </main>
 
       <footer className='[grid-area:player] min-h-[100px]'>
